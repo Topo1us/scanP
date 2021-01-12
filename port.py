0685ba1e828c6039dcca2ea692a56351b0ce1954
Topo1us <65972332+Topo1us@users.noreply.github.com>
2021-01-12 03:59:30 +0300

    Update port.py

--- port.py
+++ port.py
@@ -1,44 +1,74 @@
-import socket
-import smtplib
 import os
-os.system('clear')
+import socket
+import requests
+os.system('cls')
+print('{clear window}')
+print('{import modul socket}')
+color_red='\033[31m'
+color_green='\033[32m'
+po='port '
+cl=color_red+' close'
+b=0
+nm=0
+try:
+    requests.get("https://google.com", timeout=4)
+    print('Internet open')
+except:
+    print('Internet close')
+fg=[21, 22, 23, 25,31,41, 43, 45, 53,59, 68, 79, 80, 99, 110, 113, 115, 119, 121, 123, 135, 139, 143, 161, 179, 220, 389, 421, 443, 445, 456, 531, 555, 666, 911,
+                         993, 999, 1001, 1010, 1011,1012,1015,1024,1024,1042,1045,1090,1170,1243,1245,1269,1492,1509,1600, 1723,1807,1981,1999,2023,2115, 2140, 2155, 2283, 2600, 2801, 3024,
+                         2049, 3128, 3129, 3150, 3459, 3700, 3791, 4092, 3459, 3700, 3791,3306, 3389, 4321, 4567, 4590, 5000, 5001, 5011, 5031, 5321, 5400, 5401, 5550, 5555, 5556, 5060, 8080,
+                         5569, 5631, 5742, 6400, 6669, 5631, 5742, 6400, 6669, 6670, 6771, 6776, 6912, 6939, 6970, 7000, 7300, 7301, 7306, 7307, 7308, 7789, 9091, 9400, 9090, 9872, 9873, 9874,
+                         9875, 9876, 9878, 9989, 10101, 10520, 10607, 11000, 11225, 12076, 12223, 12345, 12346, 12361, 12362, 12631, 13000, 16969, 17300, 20000, 20001, 20203, 21544, 22222, 23456,
+                         23476, 23477, 27374, 30029, 30100, 30101, 30102, 30103, 30999, 31336, 31337, 31339, 31666, 31785, 31787, 31789, 31791, 33333, 33911, 34324, 33911, 40412, 40421, 40422, 40423,
+                         40426, 50505, 50766, 53001, 54320, 54321, 60000, 61466, 65000, 1349, 2989, 3801, 10067, 10167, 26274, 29891, 31337, 31338, 31789, 31791, 47262,54321]
 def openports(ip):
-    for port in [21, 22, 23, 25, 43, 45, 53, 68, 80, 110, 115, 119, 123, 135, 139, 143, 161, 179, 220, 389, 443, 445,
-                 993, 1723, 2049, 3306, 3389, 5060, 8080, 9091, 9090, 1164]:
+    x=1
+    port_open=0
+    port_close=0
+    op=[]
+    ops=[]
+    for port in fg:
         try:
             sock = socket.socket()
-            sock.settimeout(0.7)
+            sock.settimeout(0.1)
             sock.connect((ip, port))
-            print('\033[32mпорт:: %s' % port, ':: открыт')
+            print('\033[32mport:: %s' % port, ':: оpen.')
+            port_open+=1
+            op.append(port)
             sock.close()
         except:
-            print('\033[31mпорт:: %s' % port, ':: закрыт')
-    xw=input('~ ')
+            port_close+=1
+            ops.append(port)
+    print(color_red+'close ports '+str(port_close))
+    print(color_green+'open ports ',op)
+    if 80 not in op:
+        print('\033[33mfor ip::',ip,'no open port 80.\ntry restart.')
 def openports2():
     ip=input('IP: ')
     port=int(input('PORT: '))
     for lol in[port]:
         try:
             sock = socket.socket()
-            sock.settimeout(0.7)
+            sock.settimeout(0.2)
             sock.connect((ip, lol))
-            print('\033[32mпорт:: %s' % lol, ':: открыт')
+            print('\033[32mport:: %s' % lol, ':: open.')
             sock.close()
         except:
-            print('\033[31mпорт:: %s' % lol, ':: закрыт')
-    xc=input('~ ')
+            print('\033[31mport:: %s' % lol, ':: close.')
+            print('\033[32mdone.')
 print('''
-\033[33mВыполнено........Termux Cod
-\033[36mАвторы...........\033[31m\033[3mWolFak...\033[35m\033[3mTopo1us
-\033[32m\033[0mСообщество вк...https://m.vk.com/termux_cod
-''')
-print('''
-\033[37m[1]Сканирование стандартных портов.
-\033[37m[2]Сканирование определенного порта. 
+[1] scan standart ports.
+[2] scan certain port. 
 
 ''')
-x=input('\033[~ ')
-if x=='1':
-	openports(input('IP: '))
-if x=='2':
-	openports2()
+while True:
+    x=input('~ ')
+    if x=='1':
+        openports(input('IP: '))
+        break
+    elif x=='2':
+        openports2()
+        break
+    else:
+        print('error 404.')

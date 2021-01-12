095cf1212496710fc27553b8bd0542dbd0350980
Topo1us <65972332+Topo1us@users.noreply.github.com>
2021-01-12 15:55:22 +0300

    Update port.py

--- port.py
+++ port.py
@@ -2,20 +2,20 @@ import os
 import socket
 import requests
 os.system('clear')
-print('{clear window}')
-print('{import modul socket,requests,os}')
-print('{ver 12.01.21}')
+print('{очистил экран}')
+print('{импорт модулей socket,requests,os}')
+print('{версия 12.01.21}')
 color_red='\033[31m'
 color_green='\033[32m'
-po='port '
-cl=color_red+' close'
+po='порт '
+cl=color_red+' закрыт'
 b=0
 nm=0
 try:
     requests.get("https://google.com", timeout=4)
-    print('Internet open')
+    print('интернет подключен')
 except:
-    print('Internet close')
+    print('интернет отсутствует')
 fg=[21, 22, 23, 25,31,41, 43, 45, 53,59, 68, 79, 80, 99, 110, 113, 115, 119, 121, 123, 135, 139, 143, 161, 179, 220, 389, 421, 443, 445, 456, 531, 555, 666, 911,
                          993, 999, 1001, 1010, 1011,1012,1015,1024,1024,1042,1045,1090,1170,1243,1245,1269,1492,1509,1600, 1723,1807,1981,1999,2023,2115, 2140, 2155, 2283, 2600, 2801, 3024,
                          2049, 3128, 3129, 3150, 3459, 3700, 3791, 4092, 3459, 3700, 3791,3306, 3389, 4321, 4567, 4590, 5000, 5001, 5011, 5031, 5321, 5400, 5401, 5550, 5555, 5556, 5060, 8080,
@@ -32,9 +32,9 @@ def openports(ip):
     for port in fg:
         try:
             sock = socket.socket()
-            sock.settimeout(0.1)
+            sock.settimeout(0.3)
             sock.connect((ip, port))
-            print('\033[32mport:: %s' % port, ':: оpen.')
+            print('\033[32mport:: %s' % port, ':: открыт')
             port_open+=1
             op.append(port)
             sock.close()
@@ -44,7 +44,7 @@ def openports(ip):
     print(color_red+'close ports '+str(port_close))
     print(color_green+'open ports ',op)
     if 80 not in op:
-        print('\033[33mfor ip::',ip,'no open port 80.\ntry restart.')
+        print('\033[33mдля IP::',ip,'не открыт порт 80.\nсоветую перепроверить')
 def openports2():
     ip=input('IP: ')
     port=int(input('PORT: '))
@@ -53,14 +53,14 @@ def openports2():
             sock = socket.socket()
             sock.settimeout(0.2)
             sock.connect((ip, lol))
-            print('\033[32mport:: %s' % lol, ':: open.')
+            print('\033[32mпорт:: %s' % lol, ':: открыт')
             sock.close()
         except:
-            print('\033[31mport:: %s' % lol, ':: close.')
-            print('\033[32mdone.')
+            print('\033[31mпортt:: %s' % lol, ':: закрыт')
+            print('\033[32mготово')
 print('''
-[1] scan standart ports.
-[2] scan certain port. 
+[1] сканирование стандартных портов
+[2] сканирование определенного порта 
 
 ''')
 while True:
